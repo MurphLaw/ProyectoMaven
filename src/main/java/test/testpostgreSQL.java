4d24af89dcc56e7d45702d51a5fc77b5a09bc7a2
Gabriel Salazar <gabrielsameth@gmail.com>
2021-11-23 17:57:19 -0500

    lol

---
+++ src/main/java/test/testpostgreSQL.java
@@ -0,0 +1,9 @@
+
+package test;
+
+public class testpostgreSQL {
+    public static void main(String[] args) {
+        String url="jdbc:postresql://localhost:5432/PostresIntento";
+        
+    }
+}
