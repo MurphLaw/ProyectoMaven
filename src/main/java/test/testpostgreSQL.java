e2dd583826570fe60e0cbf280e9fc0fb160f50a6
Gabriel Salazar <gabrielsameth@gmail.com>
2021-11-23 18:35:09 -0500

    primercommit

--- src/main/java/test/testpostgreSQL.java
+++
@@ -1,9 +0,0 @@
-
-package test;
-
-public class testpostgreSQL {
-    public static void main(String[] args) {
-        String url="jdbc:postresql://localhost:5432/PostresIntento";
-        
-    }
-}
